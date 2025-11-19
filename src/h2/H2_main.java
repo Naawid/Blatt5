9820e823d0ba072ab711aac594e55dfa96b09f28
Nawid Adel <nawid@MacBook-Air-von-Nawid.local>
2025-11-19 15:27:11 +0100

    h2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -4,40 +4,22 @@ import java.util.Arrays;
 
 public class H2_main {
 	public static void main(String[] args) {
-		int n = 20;
-		int digits = 500;
+		int n = 3863;
+		int digits = String.valueOf(n).length();
 		int[] a = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		int b = 1;
-		int r;
 
-		for (int i = 1; i < 1000000000; i = i * 10) {
-			if (n - i >= 0) {
-				digits = b;
-			}
-			b++;
-		}
-		if(digits==1) {
-		for (int i = 0; i<=9; i++) {
-			if(n - i == 0) {
-				a[8]= i;
-			}
-		}
-		}
-		if(digits==2) {
-			for (int i = 10; i<=90; i++) {
-				if(n - i == 0) {
-					a[7]= i;
-				}
-			}
-			}
-		
-		
-		
-		
-		/* for (int i = digits - 1; i <= digits && i >= 0; i--) {
-			a[i] = 4;
-		}*/
+		int temp = n;
+		int index = 8;
 
+		if (n == 0) {
+			a[8] = 0;
+		} else {
+			while (temp > 0) {
+				a[index] = temp % 10;
+				temp /= 10;
+				index--;
+			}
+		}
 		System.out.println(digits);
 		System.out.println(Arrays.toString(a));
 
